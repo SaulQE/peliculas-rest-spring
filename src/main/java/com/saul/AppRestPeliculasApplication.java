eeb078fdfa554096a1465724eb3fd9c77565b455
SaulQE <saul.esquivel.028@gmail.com>
2024-04-21 19:23:28 -0500

    save

---
+++ src/main/java/com/saul/AppRestPeliculasApplication.java
@@ -0,0 +1,13 @@
+package com.saul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppRestPeliculasApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppRestPeliculasApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/saul/AppRestPeliculasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppRestPeliculasApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
