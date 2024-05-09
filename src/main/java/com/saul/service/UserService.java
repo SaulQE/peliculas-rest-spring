2a438c8839a8164e0cae4aa4aed6c35910751571
SaulQE <saul.esquivel.028@gmail.com>
2024-05-09 01:22:36 -0500

    adds services

--- src/main/java/com/saul/service/UserService.java
+++ src/main/java/com/saul/service/UserService.java
@@ -3,6 +3,7 @@ package com.saul.service;
 import com.saul.entity.User;
 
 import java.util.Collection;
+import java.util.Optional;
 
 public interface UserService {
 
@@ -11,5 +12,7 @@ public interface UserService {
     public abstract void delete(Integer userId);
     public abstract User findById(Integer userId);
     public abstract Collection<User> findAll();
+    public abstract Optional<User> findByUsername(String username);
+    public abstract void insertUserDefault(User user);
 
 }
